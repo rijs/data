d0b869ed9b0e6b78cc8c707566aaf161a8180098
pemrouz <p3pedram@hotmail.com>
2018-04-16 22:43:41 +0100

    2.2.3

--- client.bundle.js
+++ client.bundle.js
@@ -89,9 +89,21 @@ var data = (function () {
 	  }
 	}
 
+	var is$1 = /*#__PURE__*/Object.freeze({
+		default: is_1,
+		__moduleExports: is_1
+	});
+
+	var is$2 = ( is$1 && is_1 ) || is$1;
+
 	var keys = function keys(o) { 
-	  return Object.keys(is_1.obj(o) || is_1.fn(o) ? o : {})
+	  return Object.keys(is$2.obj(o) || is$2.fn(o) ? o : {})
 	};
+
+	var keys$1 = /*#__PURE__*/Object.freeze({
+		default: keys,
+		__moduleExports: keys
+	});
 
 	var copy = function copy(from, to){ 
 	  return function(d){ 
@@ -99,14 +111,28 @@ var data = (function () {
 	  }
 	};
 
+	var copy$1 = /*#__PURE__*/Object.freeze({
+		default: copy,
+		__moduleExports: copy
+	});
+
+	var dir = ( keys$1 && keys ) || keys$1;
+
+	var copy$2 = ( copy$1 && copy ) || copy$1;
+
 	var overwrite = function overwrite(to){ 
 	  return function(from){
-	    keys(from)
-	      .map(copy(from, to));
+	    dir(from)
+	      .map(copy$2(from, to));
 	        
 	    return to
 	  }
 	};
+
+	var overwrite$1 = /*#__PURE__*/Object.freeze({
+		default: overwrite,
+		__moduleExports: overwrite
+	});
 
 	var wrap = function wrap(d){
 	  return function(){
@@ -114,55 +140,81 @@ var data = (function () {
 	  }
 	};
 
+	var wrap$1 = /*#__PURE__*/Object.freeze({
+		default: wrap,
+		__moduleExports: wrap
+	});
+
 	var str = function str(d){
 	  return d === 0 ? '0'
 	       : !d ? ''
-	       : is_1.fn(d) ? '' + d
-	       : is_1.obj(d) ? JSON.stringify(d)
+	       : is$2.fn(d) ? '' + d
+	       : is$2.obj(d) ? JSON.stringify(d)
 	       : String(d)
 	};
 
+	var str$1 = /*#__PURE__*/Object.freeze({
+		default: str,
+		__moduleExports: str
+	});
+
+	var wrap$2 = ( wrap$1 && wrap ) || wrap$1;
+
+	var str$2 = ( str$1 && str ) || str$1;
+
 	var key = function key(k, v){ 
 	  var set = arguments.length > 1
-	    , keys$$1 = is_1.fn(k) ? [] : str(k).split('.').filter(Boolean)
-	    , root = keys$$1.shift();
+	    , keys = is$2.fn(k) ? [] : str$2(k).split('.').filter(Boolean)
+	    , root = keys.shift();
 
 	  return function deep(o, i){
 	    var masked = {};
 	    
 	    return !o ? undefined 
-	         : !is_1.num(k) && !k ? (set ? replace(o, v) : o)
-	         : is_1.arr(k) ? (k.map(copy), masked)
-	         : o[k] || !keys$$1.length ? (set ? (o[k] = is_1.fn(v) ? v(o[k], i) : v, o)
-	                                       :  (is_1.fn(k) ? k(o) : o[k]))
-	                                : (set ? (key(keys$$1.join('.'), v)(o[root] ? o[root] : (o[root] = {})), o)
-	                                       :  key(keys$$1.join('.'))(o[root]))
+	         : !is$2.num(k) && !k ? (set ? replace(o, v) : o)
+	         : is$2.arr(k) ? (k.map(copy), masked)
+	         : o[k] || !keys.length ? (set ? ((o[k] = is$2.fn(v) ? v(o[k], i) : v), o)
+	                                       :  (is$2.fn(k) ? k(o) : o[k]))
+	                                : (set ? (key(keys.join('.'), v)(o[root] ? o[root] : (o[root] = {})), o)
+	                                       :  key(keys.join('.'))(o[root]))
 
 	    function copy(k){
 	      var val = key(k)(o);
-	      val = is_1.fn(v)       ? v(val) 
+	      val = is$2.fn(v)       ? v(val) 
 	          : val == undefined ? v
 	                           : val;
 	    if (val != undefined) 
-	        { key(k, is_1.fn(val) ? wrap(val) : val)(masked); }
+	        { key(k, is$2.fn(val) ? wrap$2(val) : val)(masked); }
 	    }
 
 	    function replace(o, v) {
-	      keys(o).map(function(k){ delete o[k]; });
-	      keys(v).map(function(k){ o[k] = v[k]; });
+	      dir(o).map(function(k){ delete o[k]; });
+	      dir(v).map(function(k){ o[k] = v[k]; });
 	      return o
 	    }
 	  }
 	};
 
+	var key$1 = /*#__PURE__*/Object.freeze({
+		default: key,
+		__moduleExports: key
+	});
+
+	var key$2 = ( key$1 && key ) || key$1;
+
 	var header = function header(header$1, value) {
 	  var getter = arguments.length == 1;
 	  return function(d){ 
 	    return !d || !d.headers ? null
-	         : getter ? key(header$1)(d.headers)
-	                  : key(header$1)(d.headers) == value
+	         : getter ? key$2(header$1)(d.headers)
+	                  : key$2(header$1)(d.headers) == value
 	  }
 	};
+
+	var header$1 = /*#__PURE__*/Object.freeze({
+		default: header,
+		__moduleExports: header
+	});
 
 	var not = function not(fn){
 	  return function(){
@@ -170,36 +222,53 @@ var data = (function () {
 	  }
 	};
 
+	var not$1 = /*#__PURE__*/Object.freeze({
+		default: not,
+		__moduleExports: not
+	});
+
+	var not$2 = ( not$1 && not ) || not$1;
+
 	var extend = function extend(to){ 
 	  return function(from){
-	    keys(from)
-	      .filter(not(is_1.in(to)))
-	      .map(copy(from, to));
+	    dir(from)
+	      .filter(not$2(is$2.in(to)))
+	      .map(copy$2(from, to));
 
 	    return to
 	  }
 	};
+
+	var extend$1 = /*#__PURE__*/Object.freeze({
+		default: extend,
+		__moduleExports: extend
+	});
 
 	var merge_1 = merge;
 
 	function merge(to){ 
 	  return function(from){
 	    for (x in from) 
-	      { is_1.obj(from[x]) && is_1.obj(to[x])
+	      { is$2.obj(from[x]) && is$2.obj(to[x])
 	        ? merge(to[x])(from[x])
 	        : (to[x] = from[x]); }
 	    return to
 	  }
 	}
 
+	var merge$1 = /*#__PURE__*/Object.freeze({
+		default: merge_1,
+		__moduleExports: merge_1
+	});
+
 	var attr = function attr(name, value) {
 	  var args = arguments.length;
 	  
-	  return !is_1.str(name) && args == 2 ? attr(arguments[1]).call(this, arguments[0])
-	       : !is_1.str(name) && args == 3 ? attr(arguments[1], arguments[2]).call(this, arguments[0])
+	  return !is$2.str(name) && args == 2 ? attr(arguments[1]).call(this, arguments[0])
+	       : !is$2.str(name) && args == 3 ? attr(arguments[1], arguments[2]).call(this, arguments[0])
 	       :  function(el){
 	            var ctx = this || {};
-	            el = ctx.nodeName || is_1.fn(ctx.node) ? ctx : el;
+	            el = ctx.nodeName || is$2.fn(ctx.node) ? ctx : el;
 	            el = el.node ? el.node() : el;
 	            el = el.host || el;
 
@@ -209,6 +278,11 @@ var data = (function () {
 	                && el.attributes.getNamedItem(name).value
 	          } 
 	};
+
+	var attr$1 = /*#__PURE__*/Object.freeze({
+		default: attr,
+		__moduleExports: attr
+	});
 
 	var promise_1 = promise;
 
@@ -225,31 +299,59 @@ var data = (function () {
 	  return p
 	}
 
+	var promise$1 = /*#__PURE__*/Object.freeze({
+		default: promise_1,
+		__moduleExports: promise_1
+	});
+
 	var flatten = function flatten(p,v){ 
 	  if (v instanceof Array) { v = v.reduce(flatten, []); }
-	  return p = p || [], p.concat(v) 
+	  return (p = p || []), p.concat(v) 
 	};
+
+	var flatten$1 = /*#__PURE__*/Object.freeze({
+		default: flatten,
+		__moduleExports: flatten
+	});
 
 	var has = function has(o, k) {
 	  return k in o
 	};
 
+	var has$1 = /*#__PURE__*/Object.freeze({
+		default: has,
+		__moduleExports: has
+	});
+
+	var has$2 = ( has$1 && has ) || has$1;
+
 	var def = function def(o, p, v, w){
 	  if (o.host && o.host.nodeName) { o = o.host; }
 	  if (p.name) { v = p, p = p.name; }
-	  !has(o, p) && Object.defineProperty(o, p, { value: v, writable: w });
+	  !has$2(o, p) && Object.defineProperty(o, p, { value: v, writable: w });
 	  return o[p]
 	};
+
+	var def$1 = /*#__PURE__*/Object.freeze({
+		default: def,
+		__moduleExports: def
+	});
+
+	var promise$2 = ( promise$1 && promise_1 ) || promise$1;
+
+	var flatten$2 = ( flatten$1 && flatten ) || flatten$1;
+
+	var def$2 = ( def$1 && def ) || def$1;
 
 	var noop = function(){};
 
 	var emitterify = function emitterify(body, hooks) {
 	  body = body || {};
 	  hooks = hooks || {};
-	  def(body, 'emit', emit, 1);
-	  def(body, 'once', once, 1);
-	  def(body, 'off', off, 1);
-	  def(body, 'on', on, 1);
+	  def$2(body, 'emit', emit, 1);
+	  def$2(body, 'once', once, 1);
+	  def$2(body, 'off', off, 1);
+	  def$2(body, 'on', on, 1);
 	  body.on['*'] = body.on['*'] || [];
 	  return body
 
@@ -264,7 +366,7 @@ var data = (function () {
 	    for (var i = 0; i < body.on['*'].length; i++)
 	      { results.push(call(body.on['*'][i], [type, pm])); }
 
-	    return results.reduce(flatten, [])
+	    return results.reduce(flatten$2, [])
 	  }
 
 	  function call(cb, pm){
@@ -314,7 +416,7 @@ var data = (function () {
 
 	  function observable(parent, opts) {
 	    opts = opts || {};
-	    var o = emitterify(opts.base || promise_1());
+	    var o = emitterify(opts.base || promise$2());
 	    o.i = 0;
 	    o.li = [];
 	    o.fn = opts.fn;
@@ -397,38 +499,45 @@ var data = (function () {
 	  }
 	};
 
+	var emitterify$1 = /*#__PURE__*/Object.freeze({
+		default: emitterify,
+		__moduleExports: emitterify
+	});
+
+	var emitterify$2 = ( emitterify$1 && emitterify ) || emitterify$1;
+
 	var act = { add: add, update: update, remove: remove }
-	  , str$1 = JSON.stringify
+	  , str$3 = JSON.stringify
 	  , parse = JSON.parse;
 
 	var set = function set(d, skipEmit) {
 	  return function(o, existing, max) {
-	    if (!is_1.obj(o) && !is_1.fn(o))
+	    if (!is$2.obj(o) && !is$2.fn(o))
 	      { return o }
 
-	    if (!is_1.obj(d)) { 
+	    if (!is$2.obj(d)) { 
 	      var log = existing || o.log || []
 	        , root = o;
 
-	      if (!is_1.def(max)) { max = log.max || 0; }
+	      if (!is$2.def(max)) { max = log.max || 0; }
 	      if (!max)    { log = []; }
 	      if (max < 0) { log = log.concat(null); }
 	      if (max > 0) {
-	        var s = str$1(o);
+	        var s = str$3(o);
 	        root = parse(s); 
 	        log = log.concat({ type: 'update', value: parse(s), time: log.length });
 	      } 
 
-	      def(log, 'max', max);
+	      def$2(log, 'max', max);
 	      
 	      root.log 
 	        ? (root.log = log)
-	        : def(emitterify(root, null), 'log', log, 1);
+	        : def$2(emitterify$2(root, null), 'log', log, 1);
 
 	      return root
 	    }
 
-	    if (is_1.def(d.key)) {
+	    if (is$2.def(d.key)) {
 	      if (!apply(o, d.type, (d.key = '' + d.key).split('.').filter(Boolean), d.value))
 	        { return false }
 	    } else
@@ -461,14 +570,14 @@ var data = (function () {
 	}
 
 	function add(o, k, v) {
-	  is_1.arr(o) 
+	  is$2.arr(o) 
 	    ? o.splice(k, 0, v) 
 	    : (o[k] = v);
 	}
 
 	function update(o, k, v) {
-	  if (!is_1.num(k) && !k) {
-	    if (!is_1.obj(v)) { return true }
+	  if (!is$2.num(k) && !k) {
+	    if (!is$2.obj(v)) { return true }
 	    for (var x in o) { delete o[x]; }
 	    for (var x in v) { o[x] = v[x]; }
 	  } else 
@@ -476,10 +585,15 @@ var data = (function () {
 	}
 
 	function remove(o, k, v) { 
-	  is_1.arr(o) 
+	  is$2.arr(o) 
 	    ? o.splice(k, 1)
 	    : delete o[k];
 	}
+
+	var set$1 = /*#__PURE__*/Object.freeze({
+		default: set,
+		__moduleExports: set
+	});
 
 	var to = { 
 	  arr: toArray
@@ -500,20 +614,45 @@ var data = (function () {
 
 	  function reduce(p,v,i){
 	    if (i === 0) { p = {}; }
-	    p[is_1.fn(by) ? by(v, i) : v[by]] = v;
+	    p[is$2.fn(by) ? by(v, i) : v[by]] = v;
 	    return p
 	  }
 	}
+	var to_1 = to.arr;
+	var to_2 = to.obj;
+
+	var to$1 = /*#__PURE__*/Object.freeze({
+		default: to,
+		__moduleExports: to,
+		arr: to_1,
+		obj: to_2
+	});
 
 	var client = typeof window != 'undefined';
 
-	var owner = client ? /* istanbul ignore next */ window : commonjsGlobal;
+	var client$1 = /*#__PURE__*/Object.freeze({
+		default: client,
+		__moduleExports: client
+	});
+
+	var client$2 = ( client$1 && client ) || client$1;
+
+	var owner = client$2 ? /* istanbul ignore next */ window : commonjsGlobal;
+
+	var owner$1 = /*#__PURE__*/Object.freeze({
+		default: owner,
+		__moduleExports: owner
+	});
+
+	var to$2 = ( to$1 && to ) || to$1;
+
+	var owner$2 = ( owner$1 && owner ) || owner$1;
 
 	var log = function log(ns){
 	  return function(d){
-	    if (!owner.console || !console.log.apply) { return d; }
-	    is_1.arr(arguments[2]) && (arguments[2] = arguments[2].length);
-	    var args = to.arr(arguments)
+	    if (!owner$2.console || !console.log.apply) { return d; }
+	    is$2.arr(arguments[2]) && (arguments[2] = arguments[2].length);
+	    var args = to$2.arr(arguments)
 	      , prefix = '[log][' + (new Date()).toISOString() + ']' + ns;
 
 	    args.unshift(prefix.grey ? prefix.grey : prefix);
@@ -521,18 +660,37 @@ var data = (function () {
 	  }
 	};
 
+	var log$1 = /*#__PURE__*/Object.freeze({
+		default: log,
+		__moduleExports: log
+	});
+
+	var overwrite$2 = ( overwrite$1 && overwrite ) || overwrite$1;
+
+	var header$2 = ( header$1 && header ) || header$1;
+
+	var extend$2 = ( extend$1 && extend ) || extend$1;
+
+	var merge$2 = ( merge$1 && merge_1 ) || merge$1;
+
+	var attr$2 = ( attr$1 && attr ) || attr$1;
+
+	var set$2 = ( set$1 && set ) || set$1;
+
+	var require$$0 = ( log$1 && log ) || log$1;
+
 	// -------------------------------------------
 	// Adds support for data resources
 	// -------------------------------------------
 	var data = function data(ripple){
-	  log$1('creating');
+	  log$2('creating');
 	  ripple
 	    .on('change.data')
 	    .filter(function (ref) {
 	      var name = ref[0];
 	      var change = ref[1];
 
-	      return header('content-type', 'application/data')(ripple.resources[name]);
+	      return header$2('content-type', 'application/data')(ripple.resources[name]);
 	  })
 	    .filter(function (ref) {
 	      var name = ref[0];
@@ -547,35 +705,35 @@ var data = (function () {
 	      return ripple
 	      .resources[name]
 	      .body
-	      .emit('change', (change || null), not(is_1.in(['bubble'])));
+	      .emit('change', (change || null), not$2(is$2.in(['bubble'])));
 	  });
 
 	  ripple.types['application/data'] = {
 	    header: 'application/data'
 	  , ext: '*.data.js'
 	  , selector: function (res) { return ("[data~=\"" + (res.name) + "\"]"); }
-	  , extract: function (el) { return (attr("data")(el) || '').split(' '); }
-	  , check: function (res) { return is_1.obj(res.body); }
+	  , extract: function (el) { return (attr$2("data")(el) || '').split(' '); }
+	  , check: function (res) { return is$2.obj(res.body); }
 	  , load: function load(res) {
 	      var exported = commonjsRequire(res.headers.path);
 	      exported = exported.default || exported;
-	      exported = is_1.fn(exported) ? exported(ripple) : exported;
+	      exported = is$2.fn(exported) ? exported(ripple) : exported;
 	      res.headers['content-type'] = this.header;
-	      ripple(merge_1(res)(exported));
+	      ripple(merge$2(res)(exported));
 	      return ripple.resources[res.name]
 	    }
 	  , parse: function parse(res){ 
 	      var existing = ripple.resources[res.name] || {};
 
-	      extend(res.headers)(existing.headers);
-	      res.body = set()(
+	      extend$2(res.headers)(existing.headers);
+	      res.body = set$2()(
 	        res.body || []
 	      , existing.body && existing.body.log
-	      , is_1.num(res.headers.log) ? res.headers.log : -1
+	      , is$2.num(res.headers.log) ? res.headers.log : -1
 	      );
-	      overwrite(res.body.on)(listeners(existing));
+	      overwrite$2(res.body.on)(listeners(existing));
 	      res.body.on('change.bubble', function (change) {
-	        ripple.emit('change', ripple.change = [res.name, change], not(is_1.in(['data'])));
+	        ripple.emit('change', ripple.change = [res.name, change], not$2(is$2.in(['data'])));
 	        delete ripple.change;
 	      });
 
@@ -593,8 +751,8 @@ var data = (function () {
 	  return ripple
 	};
 
-	var log$1 = log('[ri/types/data]')
-	    , listeners = key('body.on');
+	var log$2 = require$$0('[ri/types/data]')
+	    , listeners = key$2('body.on');
 
 	return data;
 
